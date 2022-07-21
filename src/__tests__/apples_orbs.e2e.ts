c0a068ce645f67014b4a4ccd3e7489b95423d757
savage13 <savage13@gmail.com>
2022-07-21 16:31:43 -0400

    Add tests: orb duping, 999 apples, and 999 apples + orbs (#5)
    * Add tests: orb duping, 999 apples, and 999 apples + orbs
    
    * Update tests to use "Init GameData"

---
+++ src/__tests__/apples_orbs.e2e.ts
@@ -0,0 +1,6 @@
+// Author: savage13
+const TEST = "apples_orbs";
+it(TEST, () => {
+	expect(TEST).toPassE2ESimulation();
+});
+export { };
---
+++ src/__tests__/dup_orbs.e2e.ts
@@ -0,0 +1,6 @@
+// Author: savage13
+const TEST = "dup_orbs";
+it(TEST, () => {
+	expect(TEST).toPassE2ESimulation();
+});
+export { };
