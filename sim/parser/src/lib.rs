8dbba0d4ceda73c459205601d2b7402ba8912c06
Pistonight <pistonknight@outlook.com>
2024-09-30 00:51:35 -0700

    directory structure

---
+++ sim/parser/src/lib.rs
@@ -0,0 +1,4 @@
+pub fn test_message(n: u64) -> String {
+    format!("Hello from Rust! You passed in {}", n)
+}
+
---
+++ sim/runtime/src/lib.rs
@@ -0,0 +1,5 @@
+
+pub fn get_message() -> String {
+    ist_parser::test_message(42)
+}
+
