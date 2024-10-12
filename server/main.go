364c4371daffc93bd6bd3a70707473adf596b9fb
Pistonight <pistonknight@outlook.com>
2024-10-11 19:45:44 -0700

    testing building using alpine container

--- server/main.go
+++ server/main.go
@@ -4,25 +4,24 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/log"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	// "github.com/gofiber/fiber/v2/utils"
+	// // "github.com/gofiber/fiber/v2/utils"
 )
 
 func main() {
 	DrawStart()
-
+	
 	env := ParseEnvConfig()
 	log.SetLevel(env.LogLevel)
 	log.Debugf("Set Log Level to: %s\n", env.LogLevel)
-
+	
 	// Create the app
 	app := fiber.New()
-
 	app.Use(logger.New(logger.Config{
 		DisableColors: !env.LogColor,
 	}))
-
+	
 	DrawSection("Asset Paths")
-
+	
 	DrawLine("Manual", env.ManualPath)
 	app.Get("/manual", func(ctx *fiber.Ctx) error {
 		return ctx.Redirect("/manual/index.html")
@@ -31,13 +30,13 @@ func main() {
 		Compress: true,
 		MaxAge:   3600,
 	})
-
+	
 	DrawLine("Legacy", env.LegacyPath)
 	app.Static("/legacy", env.LegacyPath, fiber.Static{
 		Compress: true,
 		MaxAge:   3600,
 	})
-
+	
 	DrawEnd()
 
 	addr := env.Address
