353f080e63382898b9319473397d86ff47e4b387
Pistonight <pistonknight@outlook.com>
2025-02-23 20:47:20 -0800

    fix formatting

--- packages/localization/scripts/check-lang-entries.ts
+++ packages/localization/scripts/check-lang-entries.ts
@@ -1,16 +1,16 @@
 // @ts-ignore
-import fs from 'node:fs';
+import fs from "node:fs";
 // @ts-ignore
-import subprocess from 'node:child_process';
-import YAML from 'js-yaml';
+import subprocess from "node:child_process";
+import YAML from "js-yaml";
 
 const LANG_TO_CHECK = ["en-US"];
 
 // Check completeness of some lang entries using TypeScript
 const TSCONFIG = {
-    "extends": "../../../../mono-dev/tsconfig/browser.json",
-    "include": ["src"]
-}
+    extends: "../../../../mono-dev/tsconfig/browser.json",
+    include: ["src"],
+};
 const DIR = "node_modules/.cache/check-lang-entries";
 const SRC_DIR = `${DIR}/src`;
 if (!fs.existsSync(SRC_DIR)) {
@@ -18,7 +18,7 @@ if (!fs.existsSync(SRC_DIR)) {
 }
 fs.writeFileSync(`${DIR}/tsconfig.json`, JSON.stringify(TSCONFIG, null, 2));
 
-LANG_TO_CHECK.forEach(lang => {
+LANG_TO_CHECK.forEach((lang) => {
     const langFile = `src/ui/${lang}.yaml`;
     const langData = YAML.load(fs.readFileSync(langFile, "utf8"));
     const parserErrorKeyInLang: string[] = [];
@@ -43,9 +43,9 @@ console.log(key); // Check the type of key with LS to see what's missing
     fs.writeFileSync(outFile, outContent);
 });
 
-const result = subprocess.spawnSync("pnpm", ["exec", "tsc", "-p", DIR], { 
+const result = subprocess.spawnSync("pnpm", ["exec", "tsc", "-p", DIR], {
     stdio: "inherit",
-    encoding: "utf8" 
+    encoding: "utf8",
 });
 if (result.status !== 0) {
     console.error("Lang entry check failed!!!");
@@ -54,4 +54,3 @@ if (result.status !== 0) {
     // @ts-ignore
     process.exit(2);
 }
-
