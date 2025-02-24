7ad2d7996f6fb83e827e5637c3e8038582858a9e
Pistonight <pistonknight@outlook.com>
2025-02-23 16:43:04 -0800

    move out intwc

--- packages/intwc/eslint.config.js
+++
@@ -1,6 +0,0 @@
-import { config } from "mono-dev/eslint";
-
-export default config({
-    ignores: [],
-    tsconfigRootDir: import.meta.dirname,
-});
--- packages/intwc/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './EditorState.ts';
-export * from './CodeEditor.tsx';
-export * from "./types.ts";
-export * from "./init.ts";
-export * from "./utils.ts";
-export * from "./language/LanguageClient.ts";
-export * from "./language/SemanticConverter.ts";
-
