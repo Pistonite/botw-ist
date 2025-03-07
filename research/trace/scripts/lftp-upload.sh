f5fc0569196a53d1abab72330f9ecc85e9d850be
Pistonight <pistonknight@outlook.com>
2025-03-06 18:41:03 -0800

    move trace tool out of the repo

--- research/trace/scripts/lftp-upload.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p -f /atmosphere/contents/01007EF00011E000/exefs
-cd /atmosphere/contents/01007EF00011E000/exefs
-mput -e target/megaton/none/trace.nso
-mput -e target/megaton/none/main.npdm
-rm -f subsdk9
-mv trace.nso subsdk9
