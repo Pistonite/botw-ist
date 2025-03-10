d7746fd4d1ec650f9928d2ec8672ca6fd6838c78
Michael Zhao <44533763+Pistonight@users.noreply.github.com>
2025-03-09 21:04:28 -0700

    remove legacy files (#77)

--- legacy/src/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module "*.lang.yaml" {
-    type StringTree = { readonly [key: string]: StringTree | string };
-    const classes: StringTree;
-    export default classes;
-}
