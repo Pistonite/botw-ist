6c6472777e2d7079b54975a94b12933408e23b42
Pistonight <pistonknight@outlook.com>
2025-03-09 21:01:51 -0700

    remove legacy files

--- legacy/src/data/saveAs.ts
+++
@@ -1,8 +0,0 @@
-import { saveAs as save } from "file-saver";
-
-export const saveAs = (content: string, filename: string): void => {
-    const blob = new Blob([content], {
-        type: "text/plain;charset=utf-8",
-    });
-    save(blob, filename);
-};
