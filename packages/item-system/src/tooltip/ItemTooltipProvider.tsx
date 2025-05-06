c99d8b528fa8a3b291981ddb2eeced5863698adc
Pistonight <pistonknight@outlook.com>
2025-05-05 22:21:51 -0700

    fix

--- packages/item-system/src/tooltip/ItemTooltipProvider.tsx
+++ packages/item-system/src/tooltip/ItemTooltipProvider.tsx
@@ -29,7 +29,13 @@ export type ItemTooltipProviderProps = {
     backgroundUrl: string;
 };
 
-/** Provider for the ItemTooltipContext */
+/**
+ * Provider for the ItemTooltipContext
+ *
+ * Since item tooltips are very volatile (they move around by the user).
+ * We really optimize the re-render. Otherwise, users will see CPU
+ * spikes when moving the mouse really quickly
+ */
 export const ItemTooltipProvider: React.FC<
     PropsWithChildren<ItemTooltipProviderProps>
 > = ({ backgroundUrl, children }) => {
@@ -70,8 +76,9 @@ export const ItemTooltipProvider: React.FC<
     const [tooltipTarget, setTooltipTarget] = useState<
         HTMLElement | undefined
     >();
+    const lastPosition = useRef<[number, number]>([0, 0]);
     const setTooltip: SetItemTooltipFn = useCallback(
-        (x, y, props, target, verbose) => {
+        (x, y, props, target, verboseNew) => {
             if (!tooltipDivRef.current) {
                 return;
             }
@@ -80,19 +87,49 @@ export const ItemTooltipProvider: React.FC<
                 tooltipDiv.style.display = "none";
                 return;
             }
+            const visibilityChanged =
+                (tooltipDiv.style.display === "none") !== !props;
             tooltipDiv.style.display = "unset";
-            // This might initially be wrong the first time
-            // the info is changed. However, most of the time, it will be
-            // called again with the correct x and y when the mouse moves.
+            // This is incorrect for the first render. See below
             positionTooltipDiv(tooltipDiv, x, y);
-            setTooltipProps(props);
-            setTooltipTarget(target);
-            setVerbose(verbose);
+            lastPosition.current = [x, y];
+            // if the tooltip target didn't change, we don't need to rerender
+            if (
+                visibilityChanged ||
+                tooltipTarget !== target ||
+                verboseNew !== verbose
+            ) {
+                setTooltipProps(props);
+                setTooltipTarget(target);
+                setVerbose(verboseNew);
+            }
         },
-        [],
+        [tooltipTarget, verbose],
     );
 
-    // hide the tooltip if the target is removed
+    // This is needed because the positioning in the setTooltip function
+    // is incorrect - this is because the tooltip div is not in the DOM yet
+    // or still has the dimension of the previous tooltip. This is usually
+    // fine on PC since the mouse moves around and the tooltip position is updated.
+    // However, on touch devices, the tooltip is shown on click and does not
+    // change position. Therefore, we need this effect to position the tooltip
+    // on the first render. Once rendered, setTooltip will handle the positioning
+    // until the tooltip changes
+    useEffect(() => {
+        if (!tooltipDivRef.current) {
+            return;
+        }
+        const tooltipDiv = tooltipDivRef.current;
+        if (!tooltipDiv.isConnected || tooltipDiv.style.display === "none") {
+            return;
+        }
+        const [x, y] = lastPosition.current;
+        positionTooltipDiv(tooltipDiv, x, y);
+    });
+
+    // auto-hide the tooltip to prevent showing the wrong tooltip
+    // for the current thing you are hovering on, because of the optimization
+    // we did
     useEffect(() => {
         if (
             !childrenContainerRef.current ||
@@ -101,22 +138,36 @@ export const ItemTooltipProvider: React.FC<
         ) {
             return;
         }
-        const observer = new MutationObserver(() => {
-            if (!tooltipTarget) {
-                observer.disconnect();
-                return;
-            }
-            if (!tooltipTarget.isConnected) {
-                observer.disconnect();
+        const hide = () => {
+            const tooltipDiv = tooltipDivRef.current;
+            if (tooltipDiv?.isConnected) {
+                tooltipDiv.style.display = "none";
+            } else {
                 setTooltipProps(undefined);
+            }
+        };
+        // 1. if the target is removed
+        const observer = new MutationObserver(() => {
+            if (tooltipTarget && !tooltipTarget.isConnected) {
+                hide();
             }
         });
         observer.observe(childrenContainerRef.current, {
             childList: true,
             subtree: true,
         });
+        // 2. if the target itself changed
+        const observer2 = new MutationObserver(() => {
+            hide();
+        });
+        observer2.observe(tooltipTarget, {
+            attributes: true,
+            childList: true,
+            subtree: true,
+        });
         return () => {
             observer.disconnect();
+            observer2.disconnect();
         };
     }, [tooltipTarget]);
 
