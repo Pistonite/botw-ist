32463965d854edf78b419bf6d965ea71404b123a
Pistonight <pistonknight@outlook.com>
2025-07-29 19:03:40 -0700

    temp

--- packages/parser/src/cir/system_meta.rs
+++ packages/parser/src/cir/system_meta.rs
@@ -37,10 +37,21 @@ impl MetaParser for SysCommandMeta {
         &mut self,
         key: &tp::String<syn::MetaKey>,
         value: Option<&syn::MetaValue>,
-        value_span: Span, // key span if value doesn't exist
+        v_span: Span,
         errors: &mut Vec<ErrorReport>,
     ) {
-        todo!()
+        super::cir_match_meta_key_value! { (key, key_str, value, v_span, errors):
+            "dlc" => required {
+                int(x) => self.commands.push(SysCommand::Dlc(x.clamp(0, 3) as u8)),
+                float(x) => self.commands.push(SysCommand::Dlc((x as i64).clamp(0, 3) as u8)),
+                string(x) => {
+                    let Some(x) = cir::enum_name::parse_dlc_version(&x) else {
+                        errors.push(cir_error!())
+
+                    };
+                }
+            },
+        }
     }
 
     fn visit_end(self, _meta: &syn::Meta, _errors: &mut Vec<ErrorReport>) -> Self::Output {
