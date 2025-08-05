be9b3279c8d5cb0d480e35c0465ee4df62c6aeaa
Pistonight <pistonknight@outlook.com>
2025-08-04 21:52:51 -0700

    update parser error translation

--- packages/localization/src/translate_error.ts
+++ packages/localization/src/translate_error.ts
@@ -23,35 +23,39 @@ export const translateParserError = (
     error: ParserError,
     translator: Translator = translateUI,
 ): string => {
-    const key = `parser.${error.type}`;
+    const errorKey = `parser.${error.type}`;
     switch (error.type) {
         case "Unexpected":
             return translateGenericError(error.data, translator);
         case "InvalidMetaValue": {
             const [metaKey, value] = error.data;
-            return translator(key, { key: metaKey, value });
+            return translator(errorKey, { key: metaKey, value });
         }
         case "InvalidCategory": {
             const category = translateCategory(error.data, translator);
-            return translator(key, { arg: category });
+            return translator(errorKey, { arg: category });
         }
         case "InvalidCategoryName": {
-            return translator(key, { arg: error.data });
+            return translator(errorKey, { arg: error.data });
         }
         case "InvalidEquipmentSlotNum": {
             const [categoryStr, num] = error.data;
             const category = translateCategory(categoryStr, translator);
-            return translator(key, { category, num });
+            return translator(errorKey, { category, num });
+        }
+        case "InvalidSystemCommand": {
+            const [key, value] = error.data;
+            return translator(errorKey, { key: key, value });
         }
         case "GdtInvalidIndex": {
             const index = error.data;
-            return translator(key, { index });
+            return translator(errorKey, { index });
         }
         default: {
             if ("data" in error) {
-                return translator(key, { arg: error.data });
+                return translator(errorKey, { arg: error.data });
             }
-            return translator(key);
+            return translator(errorKey);
         }
     }
 };
